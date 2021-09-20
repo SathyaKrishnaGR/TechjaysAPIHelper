9569a3c9cec816dd3fa38919ca31f85508da3869
SathyaKrishna <sathya.gr@iderize.in>
2021-09-20 18:22:21 +0530

    init method added

--- Sources/TechjaysAPIHelper/TechjaysAPIHelper.swift
+++ Sources/TechjaysAPIHelper/TechjaysAPIHelper.swift
@@ -1,5 +1,9 @@
 struct TechjaysAPIHelper {
     var text = "Hello, World!"
+    
+    init(appKeys: AppKeys) {
+        
+    }
     public func getAPIBasics(appKeys: AppKeys) {
         print("Test here", appKeys)
     }
