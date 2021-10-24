bf9f660dcd0c0e3095e3e068c733ae7daa8f4876
SathyaKrishna <sathya.gr@iderize.in>
2021-10-24 21:57:29 +0530

    Added static

--- Sources/TechjaysAPIHelper/TechjaysAPIHelper.swift
+++ Sources/TechjaysAPIHelper/TechjaysAPIHelper.swift
@@ -1,5 +1,5 @@
 public class TechjaysAPIHelper {
-    public let shared = TechjaysAPIHelper()
+    public static let shared = TechjaysAPIHelper()
     public func setupApiHelper(baseUrl: String, version: String) {
         URLFactory.shared.baseUrl = baseUrl
         URLFactory.shared.version = version
