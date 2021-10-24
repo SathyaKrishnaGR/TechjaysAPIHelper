d0c6a9b74b0d733d3426c29b98a019b8b2ac1da0
SathyaKrishna <sathya.gr@iderize.in>
2021-10-24 21:54:09 +0530

    shared

--- Sources/TechjaysAPIHelper/TechjaysAPIHelper.swift
+++ Sources/TechjaysAPIHelper/TechjaysAPIHelper.swift
@@ -1,4 +1,5 @@
-public struct TechjaysAPIHelper {
+public class TechjaysAPIHelper {
+    public let shared = TechjaysAPIHelper()
     public func setupApiHelper(baseUrl: String, version: String) {
         URLFactory.shared.baseUrl = baseUrl
         URLFactory.shared.version = version
