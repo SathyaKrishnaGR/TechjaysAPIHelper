1b604c0922899fcca486a772aab2c3d0b01fffa2
SathyaKrishna <sathya.gr@iderize.in>
2021-10-25 13:48:07 +0530

    token "" check

--- Sources/TechjaysAPIHelper/APIHelper/APIClient.swift
+++ Sources/TechjaysAPIHelper/APIHelper/APIClient.swift
@@ -257,7 +257,7 @@ extension APIClient {
     }
 
     public func addAuthToken(_ headers: inout HTTPHeaders) {
-        if APIStrings.APIClient.token ! = "" {
+        if APIStrings.APIClient.token != "" {
             headers[APIStrings.APIClient.authorization] = String(format: APIStrings.APIClient.token, token)
         }
         
